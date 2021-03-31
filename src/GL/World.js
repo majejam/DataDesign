2e219d4e0c6943f7d24bbf9afcec5f6d265a5e2e
Thomas Lacroix <t.f.m.lacroix@gmail.com>
2021-03-31 14:43:04 +0200

    :sparkles: added filter class

--- src/GL/World.js
+++ src/GL/World.js
@@ -5,9 +5,7 @@ import Cull from '@/GL/Cull.js'
 import Draggable from '@/GL/Draggable.js'
 import Festival from '@/GL/Festival.js'
 import Store from '@/store'
-import GUI from '@/utils/GUI.js'
-
-import * as Filters from 'pixi-filters'
+import Filters from '@/GL/Filters.js'
 
 class World {
   constructor() {
@@ -60,11 +58,11 @@ class World {
   createWorldContainer() {
     this.world.container = new Engine.PIXI.Container()
 
-    this.createFilters()
-
     this.createBackground()
 
     this.centerWorld()
+
+    this.filters = new Filters(this.world.container)
 
     Engine.$app.stage.addChild(this.world.container)
   }
@@ -94,165 +92,6 @@ class World {
     return new Engine.PIXI.Texture.from(c)
   }
 
-  createFilters() {
-    GUI.setFolder('Filters')
-
-    this.blurFilter()
-    this.CRTFilter()
-    this.TiltShift()
-
-    this.world.container.filters = [this.blur, this.tilt, this.crt]
-  }
-
-  blurFilter() {
-    this._updateBlur = this.updateBlur.bind(this)
-    this.blur = new Engine.PIXI.filters.BlurFilter(1)
-    GUI.setFolder('Blur', 'Filters')
-    GUI.addValue(
-      'Blur',
-      'enabled',
-      {
-        default: false,
-      },
-      this._updateBlur
-    )
-    GUI.addValue(
-      'Blur',
-      'intensity',
-      {
-        default: 1,
-        min: 0,
-        max: 100,
-        step: 1,
-      },
-      this._updateBlur
-    )
-    this.updateBlur()
-  }
-
-  updateBlur() {
-    this.blur.enabled = GUI.datas.Blur.enabled
-    this.blur.blur = GUI.datas.Blur.intensity
-  }
-
-  CRTFilter() {
-    this._updateCRT = this.updateCRT.bind(this)
-    this.crt = new Filters.CRTFilter({
-      lineWidth: 0,
-      noise: 0.1,
-      vignettingAlpha: 0.3,
-      time: 0.5,
-    })
-    GUI.setFolder('CRT', 'Filters')
-    GUI.addValue(
-      'CRT',
-      'enabled',
-      {
-        default: true,
-      },
-      this._updateCRT
-    )
-    GUI.addValue(
-      'CRT',
-      'noise',
-      {
-        default: 0.05,
-        min: 0,
-        max: 1,
-        step: 0.01,
-      },
-      this._updateCRT
-    )
-    GUI.addValue(
-      'CRT',
-      'vignetting',
-      {
-        default: 0.3,
-        min: 0,
-        max: 1,
-        step: 0.01,
-      },
-      this._updateCRT
-    )
-    GUI.addValue(
-      'CRT',
-      'vignettingAlpha',
-      {
-        default: 0.3,
-        min: 0,
-        max: 1,
-        step: 0.01,
-      },
-      this._updateCRT
-    )
-    GUI.addValue(
-      'CRT',
-      'vignettingBlur',
-      {
-        default: 0.3,
-        min: 0,
-        max: 1,
-        step: 0.01,
-      },
-      this._updateCRT
-    )
-    this.updateCRT()
-  }
-
-  updateCRT() {
-    this.crt.enabled = GUI.datas.CRT.enabled
-    this.crt.noise = GUI.datas.CRT.noise
-    this.crt.vignetting = GUI.datas.CRT.vignetting
-    this.crt.vignettingAlpha = GUI.datas.CRT.vignettingAlpha
-    this.crt.vignettingBlur = GUI.datas.CRT.vignettingBlur
-  }
-
-  TiltShift() {
-    this._updateTiltShift = this.updateTiltShift.bind(this)
-    this.tilt = new Filters.TiltShiftFilter({
-      blur: 30,
-      gradientBlur: 800,
-    })
-    GUI.setFolder('Tilt', 'Filters')
-    GUI.addValue(
-      'Tilt',
-      'enabled',
-      {
-        default: false,
-      },
-      this._updateTiltShift
-    )
-    GUI.addValue(
-      'Tilt',
-      'gradientBlur',
-      {
-        default: 800,
-        min: 0,
-        max: 1000,
-        step: 10,
-      },
-      this._updateTiltShift
-    )
-    GUI.addValue(
-      'Tilt',
-      'blur',
-      {
-        default: 30,
-        min: 0,
-        max: 1000,
-        step: 10,
-      },
-      this._updateTiltShift
-    )
-    this.updateTiltShift()
-  }
-
-  updateTiltShift() {
-    this.tilt.enabled = GUI.datas.Tilt.enabled
-    this.tilt.blur = GUI.datas.Tilt.blur
-    this.tilt.gradientBlur = GUI.datas.Tilt.gradientBlur
-  }
-
   centerWorld() {
     // Move container to the center
     this.world.container.x = Engine.$app.screen.width / 2
@@ -280,8 +119,6 @@ class World {
   }
 
   update() {
-    this.crt.seed = Math.random()
-    this.crt.time += 0.5
     this.updateWorldPosition()
     this.currentFestival.getNearestConcert(this.normalizeWorldPos().x, this.normalizeWorldPos().y)
   }
